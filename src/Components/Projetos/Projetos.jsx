1b3987506f6586437797e7eeca586e9dc5cc8c56
Veronica-Andre <veronicaandre.dss@gmail.com>
2026-01-26 10:33:57 -0300

    Estilização do Projeto

--- src/Components/Projetos/Projetos.jsx
+++ src/Components/Projetos/Projetos.jsx
@@ -1,8 +1,9 @@
-import {useState} from "react" //importando o hook useStates que nos permite guardar e controlar estados
-import PetAdopt from "../../assets/PetAdopt.jpg"
-import ReciclaTech from "../../assets/ReciclaTech.jpg"
-import VerdeAcao from "../../assets/VerdeAcao.jpg"
+import {useState} from "react"; //importando o hook useStates que nos permite guardar e controlar estados
+import PetAdopt from "../../assets/PetAdopt.jpg";
+import ReciclaTech from "../../assets/ReciclaTech.jpg";
+import VerdeAcao from "../../assets/VerdeAcao.jpg";
 
+import styles from "./Projetos.module.scss";
 
 function Projetos () {
 
@@ -28,30 +29,48 @@ function Projetos () {
             imagem: VerdeAcao,
             nome: "Verde Ação",
             descricao: "Projeto criado para adoção de pets"
+                 
+        },
+       
+        {
+            id: 4,
+            imagem: PetAdopt,
+            nome: "PetAdopt",
+            descricao: "Projeto criado para adoção de pets"
+            
+        }, 
+
+        {
+            id: 5,
+            imagem: ReciclaTech,
+            nome: "ReciclaTech",
+            descricao: "Projeto focado para facilitar doação de eletrônicos"
+            
+        }, 
+
+        {
+            id: 6,
+            imagem: VerdeAcao,
+            nome: "Verde Ação",
+            descricao: "Projeto criado para adoção de pets"
             
         }
     ] )
  
 
-    return(
-        <>
-
-        <h2>Projetos</h2>
-
-        <article>
-           {projetos.map((item) => (
-
-            <div key={item.id}>
-                <img src={item.imagem} alt="" />
-                <h2>{item.nome}</h2>
-                <p>{item.descricao}</p>
-                <h3> {projetos[1].nome} </h3>
-            </div>
-
-           ))}
-        </article>
-
-        </>
-    )
+    return (
+    
+      <section className={styles.sectionProjetos}>
+        {projetos.map((item) => (
+          <article key={item.id}className={styles.cardProjetos}>
+            <img src={item.imagem} alt="" />
+            <h2> {item.nome} </h2>
+            <p> {item.descricao} </p>
+           {/*<h3> {projetos[1].nome} </h3>*/}
+          </article>
+        ))}
+      </section>
+  
+  );
 }
 export default Projetos
